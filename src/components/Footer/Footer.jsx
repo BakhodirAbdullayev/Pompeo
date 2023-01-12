0515b3a0e8eda801bc9302a6646e4c5a6324c4a6
bakhodirabdullayev <bahodirabdullayev0556@gmail.com>
2023-01-12 23:18:42 +0500

    navbar toggle checkboxdan jsga uzgartirildi

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,18 +6,21 @@ import "./Footer.scss";
 function Footer() {
   const contact = [
     {
+      id: 1,
       name: "email",
       logo: "fa-solid fa-envelope",
       text: "pompeopotery@gmail.com",
       link: "mailto:pompeopotery@gmail.com",
     },
     {
+      id: 2,
       name: "find",
       logo: "fa-solid fa-location-dot",
       text: `Central Park, Manhattan New York, 1101`,
       link: "https://map.google.com",
     },
     {
+      id: 3,
       name: "call",
       logo: "fa-solid fa-phone",
       text: "+1 292 345 678",
@@ -25,21 +28,22 @@ function Footer() {
     },
   ];
   return (
-    <div class="footerSection container">
-      <a href="./index.html" class="footerLogo">
+    <div className="footerSection container">
+      <a href="./index.html" className="footerLogo">
         <img src={Logo} alt="logo" />
       </a>
-      <div class="footerTitle">
+      <div className="footerTitle">
         I have always striven to fix beauty in wood, stone, glass or pottery,
         that has been my creed.
       </div>
-      <div class="footerContact">
+      <div className="footerContact">
         {contact.map((item) => (
           <FooterContact
             name={item.name}
             logo={item.logo}
             link={item.link}
             text={item.text}
+            key={item.id}
           />
         ))}
       </div>
--- src/components/Header/HeaderNav/index.jsx
+++ src/components/Header/HeaderNav/index.jsx
@@ -1,36 +1,64 @@
-import React from 'react';
-import './HeaderNav.scss'
+import React, { useState } from "react";
+import "./HeaderNav.scss";
 
 function HeaderNav() {
-    return ( 
-        <div className="headerNav">
-          <label for="check" className="headerNavIcon">
-            <i className="fas fa-bars"></i>
-          </label>
-          <input type="checkbox" id="check" />
-          <label for="check" className="layer"></label>
-          <div className="headerNavBurgerLinks">
-            <a href="#" className="headerNavLink activeLink">home</a>
-            <a href="#" className="headerNavLink">about</a>
-            <a href="#" className="headerNavLink">shop</a>
-            <a href="#" className="headerNavLink">contact</a>
-            <a href="#" className="headerNavCart">
-              <i className="fa-solid fa-cart-shopping"></i>
-              Cart
-            </a>
-          </div>
-          <div className="headerNavLinks">
-            <a href="#" className="headerNavLink activeLink">home</a>
-            <a href="#" className="headerNavLink">about</a>
-            <a href="#" className="headerNavLink">shop</a>
-            <a href="#" className="headerNavLink">contact</a>
-            <a href="#" className="headerNavCart">
-              <i className="fa-solid fa-cart-shopping"></i>
-              Cart
-            </a>
-          </div>
-        </div>
-     );
+  const [toggle, setToggle] = useState(false);
+
+  const onToggle = () => {
+    setToggle((prev) => !prev);
+  };
+
+  return (
+    <div className="headerNav">
+      <div for="check" className="headerNavIcon">
+        <i className="fas fa-bars" onClick={onToggle}></i>
+      </div>
+      <div
+        className={toggle ? "layer navToggle" : "layer"}
+        onClick={onToggle}
+      ></div>
+      <div
+        className={
+          toggle ? "headerNavBurgerLinks navToggle" : "headerNavBurgerLinks"
+        }
+      >
+        <a href="#" className="headerNavLink activeLink">
+          home
+        </a>
+        <a href="#" className="headerNavLink">
+          about
+        </a>
+        <a href="#" className="headerNavLink">
+          shop
+        </a>
+        <a href="#" className="headerNavLink">
+          contact
+        </a>
+        <a href="#" className="headerNavCart">
+          <i className="fa-solid fa-cart-shopping"></i>
+          Cart
+        </a>
+      </div>
+      <div className="headerNavLinks">
+        <a href="#" className="headerNavLink activeLink">
+          home
+        </a>
+        <a href="#" className="headerNavLink">
+          about
+        </a>
+        <a href="#" className="headerNavLink">
+          shop
+        </a>
+        <a href="#" className="headerNavLink">
+          contact
+        </a>
+        <a href="#" className="headerNavCart">
+          <i className="fa-solid fa-cart-shopping"></i>
+          Cart
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default HeaderNav;
